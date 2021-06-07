e4303fb348c9c79ac4cc4d83d3132f836dbbaa67
N2WU <49031851+N2WU@users.noreply.github.com>
2021-06-07 14:43:43 -0600

    Update receive_LCD.ino

--- receive_LCD.ino
+++ receive_LCD.ino
@@ -1,9 +1,33 @@
 //from https://lastminuteengineers.com/nrf24l01-arduino-wireless-communication/
-//Requires a handler module for multiple I2Cs
+//Requires multiple SPIs
+//http://www.learningaboutelectronics.com/Articles/Multiple-SPI-devices-to-an-arduino-microcontroller.php
+/*
+  The circuit:
+ * LCD RS pin to digital pin 12
+ * LCD Enable pin to digital pin 11
+ * LCD D4 pin to digital pin 5
+ * LCD D5 pin to digital pin 4
+ * LCD D6 pin to digital pin 3
+ * LCD D7 pin to digital pin 2
+ * LCD R/W pin to ground
+ * LCD VSS pin to ground
+ * LCD VCC pin to 5V
+ * 10K resistor:
+ * ends to +5V and ground
+ * wiper to LCD VO pin (pin 3)
+ Use logic converter with nRF24 since you are using arduino Uno
+ */
 //Include Libraries
 #include <SPI.h>
 #include <nRF24L01.h>
 #include <RF24.h>
+#include <LiquidCrystal.h>
+
+// initialize the library by associating any needed LCD interface pin
+// with the arduino pin number it is connected to
+const int rs = 12, en = 11, d4 = 5, d5 = 4, d6 = 3, d7 = 2;
+LiquidCrystal lcd(rs, en, d4, d5, d6, d7);
+
 
 //create an RF24 object
 RF24 radio(9, 8);  // CE, CSN
@@ -23,6 +47,9 @@ void setup()
   
   //Set module as receiver
   radio.startListening();
+  lcd.begin(16, 2);
+  // Print a message to the LCD.
+  lcd.print("Initializing");
 }
 
 void loop()
@@ -33,9 +60,13 @@ void loop()
     char text[32] = {0};
     radio.read(&text, sizeof(text)); //could potentially be an issue where multiple transmissions are picked up
     Serial.println(text);
+    lcd.setCursor(0, 1);
+    // print the number of seconds since reset:
+    lcd.print(text);
     delay(250);
   }
   //add some fancy ways to read and display code, or package it up beforehand
   //https://www.hacktronics.com/Tutorials/arduino-character-lcd-tutorial.html
+  //SLOCK, MOSI, MISO all have same wiring. Should work for multiple. Look into CS/SS/CSN
   
 }
