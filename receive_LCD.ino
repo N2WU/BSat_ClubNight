9825a984d0c2d552fe0fc2f7788acdb1921dfafb
N2WU <49031851+N2WU@users.noreply.github.com>
2021-06-22 22:43:44 -0400

    fix bugs, add text wrap code

--- receive_LCD.ino
+++ receive_LCD.ino
@@ -1,6 +1,7 @@
 //from https://lastminuteengineers.com/nrf24l01-arduino-wireless-communication/
 //Requires multiple SPIs
 //http://www.learningaboutelectronics.com/Articles/Multiple-SPI-devices-to-an-arduino-microcontroller.php
+//LCD Shield: https://www.jameco.com/z/DFR0009-DFRobot-DFRobot-LCD-Shield-for-Arduino_2144411.html including pinout
 /*
   The circuit:
  * LCD RS pin to digital pin 12
@@ -25,12 +26,13 @@
 
 // initialize the library by associating any needed LCD interface pin
 // with the arduino pin number it is connected to
-const int rs = 12, en = 11, d4 = 5, d5 = 4, d6 = 3, d7 = 2;
+//const int rs = 12, en = 11, d4 = 5, d5 = 4, d6 = 3, d7 = 2;
+const int rs = 8, en = 9, d4 = 4, d5 = 5, d6 = 6, d7 = 7;
 LiquidCrystal lcd(rs, en, d4, d5, d6, d7);
 
 
 //create an RF24 object
-RF24 radio(9, 8);  // CE, CSN
+RF24 radio(3, 2);  // CE, CSN
 
 //address through which two modules communicate.
 const byte address[6] = "00001";
@@ -57,16 +59,43 @@ void loop()
   //Read the data if available in buffer
   if (radio.available())
   {
+    lcd.clear();
     char text[32] = {0};
     radio.read(&text, sizeof(text)); //could potentially be an issue where multiple transmissions are picked up
     Serial.println(text);
-    lcd.setCursor(0, 1);
-    // print the number of seconds since reset:
+    lcd.setCursor(0, 0);
+     //print essentially the first 16 characters on the first row of the LCD display
+   if (sizeof(text) > 15) //if it doesn't fit
+   {
+    bool DisplayFlag = false;
+    byte colcounter = 0;
+    for (byte i = 0; i < sizeof(text); i = i + 1) {
+      if ((DisplayFlag == true) && ((isDigit(text[i]) == true) || (text[i] == '.')))//put only numbers (not garbage) on the bottom
+      {
+        lcd.setCursor(colcounter,1);
+        lcd.print(text[i]);
+        colcounter++;
+      }
+      else
+      {
+        lcd.setCursor(i,0);
+        lcd.print(text[i]);
+      }
+      if (text[i] == ':')
+      {
+        DisplayFlag = true; //set it once and continue using it
+      }
+    }
+    }
+    else
+   {
     lcd.print(text);
+   }
+  }
+   
     delay(250);
   }
   //add some fancy ways to read and display code, or package it up beforehand
   //https://www.hacktronics.com/Tutorials/arduino-character-lcd-tutorial.html
   //SLOCK, MOSI, MISO all have same wiring. Should work for multiple. Look into CS/SS/CSN
   
-}
